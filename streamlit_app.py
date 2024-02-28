8f6f49c4986ffc8d02094172227b5a9080efce69
AkmaralSeyidova <160583410+AkmaralSeyidova@users.noreply.github.com>
2024-02-28 20:39:45 +0200

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,42 @@
 import streamlit as st
-import streamlit as st
-
+import joblib
 # Title of the application
-st.title("My Streamlit App")
+st.title("Car Price Prediction App")
 
 # Header
-st.header("Welcome to my app!")
+st.header("Predict the Price of Your Car")
 
 # Subheader
-st.subheader("This is a demo of Streamlit")
-# Display Images
+st.subheader("Learn the value of your car using this application.")
 
-# import Image from pillow to open images
-from PIL import Image
-img = Image.open("car.jpeg")
+st.image("car_img.jpg", caption="Blue Car", width=400)
 
-# display image using streamlit
-# width is used to set the width of an image
-st.image(img, width=200)
+st.write("You can see the estimated market value of the car you are interested in below.")
+
+columns = joblib.load("features_list.joblib")
+
+min_year = 2003
+max_year = 2018
+year = st.number_input("Year:", min_value=min_year, max_value=max_year)
+
+min_present_price = 0.1
+max_present_price = 100
+Present_price = st.slider("Present_price:", min_value=min_present_price, max_value=max_present_price)
+
+if fuel_type == "Benzin":
+    fuel = 'Petrol'
+elif fuel_type == 'Dizel':
+    fuel = 'Diesel'
+else:
+    fuel = 'CNG'
+
+st.write('You selected:', fuel_type)
+
+seller_type = st.selectbox(
+    'Owner:',
+    ['Galeri', 'Sahibinden'])
+
+if seller_type == 'Galeri':
+    seller = 'Dealer'
+elif seller_type ==  'Sahibinden':
+    seller = 'Individual'
